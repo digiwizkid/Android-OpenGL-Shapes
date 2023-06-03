cd5b1f7bf1a0f48b2ecc5aab75092e408e0997e1
digiwizkid <digiwizkid@gmail.com>
2023-06-03 12:37:41 +0530

    package update

--- app/src/androidTest/java/com/digiwizkid/OpenGlShapes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/digiwizkid/OpenGlShapes/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.digiwizkid.indianflagopengl;
+package com.digiwizkid.OpenGlShapes;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.digiwizkid.indianflagopengl", appContext.getPackageName());
+        assertEquals("com.digiwizkid.OpenGlShapes", appContext.getPackageName());
     }
 }
